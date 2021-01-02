0748f45940fe5654a458f38856d7491fd1da2a47
Maki Raita <maki.raita@gmail.com>
2021-01-02 23:12:44 +0900

    Add main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        ::std::thread::sleep(Duration::from_secs(1));
+        println!("yay");
+    };
 }
